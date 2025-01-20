50d3b12a6b6cc268d9cd54333ea8ed0b94a39e25
lsd7nk <87329383+ElovikovSergei@users.noreply.github.com>
2025-01-21 00:44:11 +0700

    add set loading progress

--- Assets/App/Modules/Common/Views/LoadingView.cs
+++ Assets/App/Modules/Common/Views/LoadingView.cs
@@ -1,8 +1,16 @@
+using Doozy.Engine.Progress;
+using UnityEngine;
+
 namespace App.Common.Views
 {
     public sealed class LoadingView : StateView
     {
-        //[Header("Loading")]
-        //[SerializeField] private 
+        [Header("Loading")]
+        [SerializeField] private Progressor _progressor;
+
+        public void SetProgress(float value)
+        {
+            _progressor.SetValue(value);
+        }
     }
 }
